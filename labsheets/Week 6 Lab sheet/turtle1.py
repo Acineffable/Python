a6aaad681abc364f4caae66edc63041733d84478
fiariii <35525102+fiariii@users.noreply.github.com>
2018-12-18 15:21:16 +0000

    Add files via upload

---
+++ labsheets/Week 6 Lab sheet/turtle1.py
@@ -0,0 +1,12 @@
+import turtle
+myScreen = turtle.Screen()
+
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+myScreen.mainloop()
+
